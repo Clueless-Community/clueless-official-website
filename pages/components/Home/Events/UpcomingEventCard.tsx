71edd4fb0f46ac3bcab3595ae377430099726851
Debajyoti <debajyotisaha14@gmail.com>
2022-08-02 01:54:24 +0530

    Try4

--- pages/components/Home/Events/UpcomingEventCard.tsx
+++ pages/components/Home/Events/UpcomingEventCard.tsx
@@ -67,7 +67,9 @@ const UpcomingEventCard: React.FC<dataProps> = ({ eventposter, heading, venue, T
             <h1 className='xl:text-4xl text-2xl xl:hidden font-semibold'> {heading}</h1>
             <div className={`flex flex-col  xl:hidden text-xl`}>
                 <h1 className='font-semibold'>Instructor / speaker :</h1>
-                <h1>{instructorOrspeaker[0]?.name}</h1>
+                {allSpeakerNames?.map((item, i) => {
+                    return <h1 key={i}>{item}</h1>
+                })}
             </div>
             <div>
                 {session ? <Link href={`/events/${heading}`}>
