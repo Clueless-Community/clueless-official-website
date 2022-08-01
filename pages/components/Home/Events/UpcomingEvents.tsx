fa77a8020dd8d7b0981bf0c37c79b158dcf69df8
Rajdeep <rajdipgupta019@gmail>
2022-08-02 01:10:34 +0530

    try -1

--- pages/components/Home/Events/UpcomingEvents.tsx
+++ pages/components/Home/Events/UpcomingEvents.tsx
@@ -9,10 +9,10 @@ const UpcomingEvents = () => {
     <div className=''>
       <h1 className='text-4xl text-center py-8 font-semibold' >Upcoming Events </h1>
       <div className=''>
-        {eventData.map((data, i) => {
+        {/* {eventData.map((data, i) => {
           return <UpcomingEventCard eventposter={data.eventposter} heading={data.heading} venue={data.venue} startingTime={data.startingTime} endingTime={data.endingTime} instructorOrspeaker={data.instructorOrspeaker}
             attractions={data.attractions} agenda={data.agenda} />
-        })}
+        })} */}
 
       </div>
     </div>
