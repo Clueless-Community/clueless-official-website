4806231e5592871962dabf38d3813bbc5b6b4ff0
Rajdeep <rajdipgupta019@gmail>
2022-08-01 15:29:51 +0530

    try-2

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { collection, getDocs } from "firebase/firestore";
-import { db } from '../../firebase/clientApp';
+import { db } from '../../lib/clientApp';
 import PreviousEventCard from '../components/Home/Events/PreviousEventCard';
 
 const index = () => {
