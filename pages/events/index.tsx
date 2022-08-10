955cbf38b2b229306220847aa8ae17ec80bd9ab7
Debajyoti <debajyotisaha14@gmail.com>
2022-08-10 17:09:55 +0530

    Events page empty state feature added

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -11,15 +11,16 @@ import { format } from 'date-fns';
 
 const Events = () => {
     const [eventData, seteventData] = React.useState<any[]>([])
+    const [isFetched, setisFetched] = React.useState<boolean>(false)
 
     const getEventData = React.useCallback(async () => {
         const data: ((prevState: never[]) => never[]) | { id: string; }[] = []
-        getDocs(collection(db, 'events')).then((snapshot: { docs: any[]; }) => {
+        await getDocs(collection(db, 'events')).then((snapshot: { docs: any[]; }) => {
             snapshot.docs.map((doc) => {
                 data?.push({ ...doc.data(), id: doc.id })
             })
             seteventData(data)
-
+            setisFetched(true)
         }).catch((err: any) => {
             console.log(err);
         })
@@ -30,24 +31,30 @@ const Events = () => {
     }, [getEventData])
 
     console.log(eventData);
-
-
     return (
         <div>
-            {eventData.length > 0 ?
+            {isFetched ?
                 <div>
                     <Navbar />
                     <Swag />
 
                     <div className='xl:my-20 my-4 md:mx-40 mx-4'>
                         <h1 className='text-4xl text-center py-8 font-semibold' >Upcoming Events </h1>
-                        <div className='space-y-4'>
-                            {eventData.map((data, i) => {
-                                const date = format(new Date(data.date.seconds * 1000), 'do LLLLLL, yyyy')
-                                return <UpcomingEventCard key={i} eventposter={data.event_icon_image} heading={data.event_name} venue={data.venue_name} Time={data.time_period} instructorOrspeaker={data.speakers_info}
-                                    attractions="Win T-shirts, swags and free food. 🚀 " agenda={data.agenda} eventId={data.id} date={date} />
-                            })}
-                        </div>
+                        {eventData.length > 0 ? <div>
+
+                            <div className='space-y-4'>
+                                {eventData.map((data, i) => {
+                                    const date = format(new Date(data.date.seconds * 1000), 'do LLLLLL, yyyy')
+                                    return <UpcomingEventCard key={i} eventposter={data.event_icon_image} heading={data.event_name} venue={data.venue_name} Time={data.time_period} instructorOrspeaker={data.speakers_info}
+                                        attractions="Win T-shirts, swags and free food. 🚀 " agenda={data.agenda} eventId={data.id} date={date} />
+                                })}
+                            </div>
+                        </div> :
+                            <div className="flex flex-col justify-center items-center">
+                                <img src="/Calendar.svg" alt="" className="w-[250px] md:w-[400px] mt-8" />
+                                <h1 className="text-center text-lg md:text-2xl text-[#908A8A] mt-8">No Upcoming Events listed for now!</h1>
+                            </div>
+                        }
                     </div>
                     {/* <div className=''>
                     <h1 className='text-4xl text-center py-8 font-semibold' >Previous Events </h1>
