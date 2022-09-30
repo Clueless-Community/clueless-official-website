96caf5f179498a225c2e0602388373cf33958325
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-09-30 21:54:44 +0530

    Hacktoberfest Page Header Done

--- pages/components/Home/Hacktoberfest.tsx
+++ pages/components/Home/Hacktoberfest.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
+import Link from 'next/link';
 
 
 const Hacktoberfest = () => {
@@ -8,9 +9,11 @@ const Hacktoberfest = () => {
             <img src='/hacktoberfest_logo.png' className='mx-auto lg:mx-0' />
             <div className='my-auto'>
                 <h1 className='text-white text-[14px] lg:text-3xl xl:text-4xl'>Join the biggest Open-source festival with Clueless & Win Amazing Swags 🎊</h1>
-                <button className="btn-blue bg-white text-black hover:bg-gray-100 mt-10 flex items-center mx-auto lg:mx-0">
-                    <ReadMoreIcon className="mr-2" />Learn More
-                </button>
+                <Link href="/hacktoberfest" passHref>
+                    <button className="btn-blue bg-white text-black hover:bg-gray-100 mt-10 flex items-center mx-auto lg:mx-0">
+                        <ReadMoreIcon className="mr-2" />Explore
+                    </button>
+                </Link>
             </div>
         </div>
     )
