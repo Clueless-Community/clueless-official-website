3a3645280d7b88aec7ba5c80fee495da2c1a8d01
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-10-01 02:00:46 +0530

    Deployment issues fixed

--- pages/components/Hacktoberfest/Projects.tsx
+++ pages/components/Hacktoberfest/Projects.tsx
@@ -7,8 +7,8 @@ const Projects = () => {
         <div>
             <h1 className='text-center text-[24px] lg:text-3xl xl:text-4xl'>Projects 🚀</h1>
             <div className='md:grid gap-10 xl:grid-cols-2 m-10 md:mx-40 flex flex-col justify-center text-center lg:text-left rounded-3xl py-[28px] px-[20px] sm:px-[91px]'>
-                {ProjectsHacktoberFest.map(item => {
-                    return <ProjectCard name={item.Name} description={item.Description} imgSources={item.techstack_Icon} repoUrl={item.repoUrl} />
+                {ProjectsHacktoberFest.map((item, i) => {
+                    return <ProjectCard key={i} name={item.Name} description={item.Description} imgSources={item.techstack_Icon} repoUrl={item.repoUrl} />
                 })}
             </div>
 
