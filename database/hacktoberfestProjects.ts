5e451677e661a33acd39f098f53595f74a02c83f
Rajdeep Sengupta <rajdeepsengupta@Rajdeeps-MacBook-Air.local>
2022-10-01 09:29:58 +0530

    Hacktoberfest data added

--- database/hacktoberfestProjects.ts
+++ database/hacktoberfestProjects.ts
@@ -1,6 +1,6 @@
 export const ProjectsHacktoberFest = [{
     Name: 'Clueless Community Website',
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nullam fringilla est, lacus bibendum volutpat viverra mi pellentesque.',
+    Description: 'This is Clueless official website where you can make your developer profile as well as meet new people across the world.',
     techstack_Icon: [
         '/projectIcons/react.png',
         '/projectIcons/next.png',
@@ -11,45 +11,38 @@ export const ProjectsHacktoberFest = [{
 },
 {
     Name: 'First-Contribution',
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nullam fringilla est, lacus bibendum volutpat viverra mi pellentesque.',
+    Description: 'A repo where you can make your first contribution and get a contributors card in our website.',
     techstack_Icon: [
         '/projectIcons/react.png',
         '/projectIcons/next.png',
-        '/projectIcons/ts.png',
-        '/projectIcons/firebase.png',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png',
     ],
     repoUrl: 'https://github.com/Clueless-Community/first-contribution'
 },
 {
     Name: 'College API',
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nullam fringilla est, lacus bibendum volutpat viverra mi pellentesque.',
+    Description: 'An API that helps you to fetch colleges and their details.',
     techstack_Icon: [
-        '/projectIcons/react.png',
-        '/projectIcons/next.png',
-        '/projectIcons/ts.png',
-        '/projectIcons/firebase.png',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png',
+        'https://seeklogo.com/images/F/fastapi-logo-541BAA112F-seeklogo.com.png',
     ],
     repoUrl: 'https://github.com/Clueless-Community/collegeAPI'
 },
 {
     Name: 'Prega.io',
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nullam fringilla est, lacus bibendum volutpat viverra mi pellentesque.',
+    Description: 'A medical documents handling app made in flutter for pregnant women.',
     techstack_Icon: [
-        '/projectIcons/react.png',
-        '/projectIcons/next.png',
-        '/projectIcons/ts.png',
+        'https://www.kindpng.com/picc/m/355-3557482_flutter-logo-png-transparent-png.png',
+        'https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png',
         '/projectIcons/firebase.png',
     ],
     repoUrl: 'https://github.com/Clueless-Community/Prega'
 },
 {
     Name: 'Datasets',
-    Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nullam fringilla est, lacus bibendum volutpat viverra mi pellentesque.',
+    Description: 'Collection of useful datasets.',
     techstack_Icon: [
-        '/projectIcons/react.png',
-        '/projectIcons/next.png',
-        '/projectIcons/ts.png',
-        '/projectIcons/firebase.png',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/JSON_vector_logo.svg/2048px-JSON_vector_logo.svg.png',
     ],
     repoUrl: 'https://github.com/Clueless-Community/Datasets'
 },
--- pages/components/Hacktoberfest/ProjectCard.tsx
+++ pages/components/Hacktoberfest/ProjectCard.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import ReadMoreIcon from '@mui/icons-material/ReadMore';
 import Link from 'next/link';
@@ -14,14 +16,14 @@ const ProjectCard: React.FC<ProjectProps> = ({ name, description, imgSources, re
         <div className='md:w-[475px] bg-gradient-to-r from-black via-black to-neutral-400 rounded-2xl p-[57px]'>
             <h1 className='text-white text-[24px] lg:text-3xl'>{name}</h1>
             <div className='space-y-2'>
-                <p className='text-white text-[14px] text-3xl'>{description}</p>
+                <p className='text-white text-sm mt-3'>{description}</p>
                 <div className='space-x-3 flex justify-center md:justify-start'>
                     {imgSources?.map((image, i) => {
                         return <img key={i} src={image} className='rounded-full w-5 h-5' />
                     })}
                 </div>
             </div>
-            <Link href={`${repoUrl}`} passHref>
+            <Link href={repoUrl} passHref>
                 <button className="btn-blue bg-white text-black hover:bg-gray-100 mt-10 flex items-center mx-auto lg:mx-0">
                     <ReadMoreIcon className="mr-2" />Explore
                 </button>
