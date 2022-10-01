56fa1c6a02ce42571fa7e56b4e90bc3677d680c4
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-10-02 03:27:34 +0530

    Redesigned profile project card

--- pages/components/Profile/ProjectsCard.tsx
+++ pages/components/Profile/ProjectsCard.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable @next/next/no-img-element */
-import { Button, Stack } from '@mui/material'
 import React from 'react'
 import StyledChip from '../shared/StyledChip'
 import LinkIcon from '@mui/icons-material/Link';
@@ -15,44 +14,41 @@ interface Props {
 
 const ProjectsCard: React.FC<Props> = ({ projectImage, projectName, projectDesc, publicLink, gitHubLink, techStacks }) => {
     return (
-        <div className='p-6 border-2 border-dashed border-black border-opacity-60 rounded-lg'>
-            <div className='flex'>
-                <img src={projectImage} alt="ok" className='w-32 h-32 rounded-lg'  />
-                <div className='ml-5'>
-                    <div className=' flex flex-col xl:flex-row xl:items-center  '>
-                        <a href={publicLink} className='flex items-center flex-wrap'>
-                            <h1 className=' text-2xl mr-2 font-semibold hover:underline underline-offset-2 transition-all'>{projectName}</h1>
-                            <LinkIcon className=' cursor-pointer' />
-                        </a>
-                        <div className='hidden lg:block'>
-                            <div className='flex gap-2 xl:ml-10'>
-                                {techStacks?.map((teckStack: { 'name': string }, i) => {
-                                    return (
-                                        <StyledChip text={teckStack.name} size="xs" rounded='2xl'  key={i}/>
-                                    )
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                    <div className=' hidden lg:block '>
-                        <p className='mt-5 font-semibold opacity-80'>{projectDesc}</p>
-                        <a href={gitHubLink} target="_blank" rel="noreferrer">
-                            <button className='btn-blue mt-5 flex items-center px-4 py-2 shadow-blue-600'>View Repo</button>
-                        </a>
-                    </div>
+        <div className='border-opacity-60 rounded-xl shadow-lg'>
+            <div
+                className='w-full h-[14rem] rounded-t-xl'
+                style={{
+                    background: `url('${projectImage}')`,
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover"
+                }} />
+            <div className="p-5 h-[17rem] flex flex-col">
+                <a href={publicLink} className='flex flex-row items-center flex-wrap w-[100%]'>
+                    <h1 className='flex-auto text-2xl mr-2 font-semibold hover:underline underline-offset-2 transition-all'>
+                        {projectName}
+                    </h1>
+                    <LinkIcon className=' cursor-pointer' />
+                </a>
+                <div className='flex gap-2 flex-row my-2 flex-wrap'>
+                    {techStacks?.map((teckStack: { 'name': string }, i) => {
+                        return (
+                            <StyledChip text={teckStack.name} size="xs" rounded='2xl'  key={i}/>
+                        )
+                    })}
                 </div>
-            </div>
-            <div className=' block lg:hidden '>
-                <Stack direction="row" spacing={2} className="mt-5" alignItems="center" sx={{ 'flexWrap': 'wrap' }}>
-                            {techStacks?.map((teckStack: { 'name': string }, i) => {
-                                return (
-                                    <StyledChip text={teckStack.name} size="xs" rounded='2xl' key={i} />
-                                )
-                            })}
-                </Stack>
-                <p className='mt-5 font-semibold opacity-80'>{projectDesc}</p>
-                <a href={gitHubLink} target="_blank" rel="noreferrer">
-                    <button className='btn-blue mt-5 flex items-center px-4 py-2 shadow-blue-600'>View Repo</button>
+                <p className='my-2 font-semibold opacity-80 flex-auto w-full overflow-y-hidden'>
+                    {projectDesc}
+                </p>
+                <a
+                    href={gitHubLink}
+                    target="_blank"
+                    rel="noreferrer"
+                    className='btn-blue my-2 px-4 py-2 flex items-center shadow-blue-600 w-full text-center'
+                >
+                    <div className="w-full text-center">
+                        View Repo
+                    </div>
                 </a>
             </div>
         </div>
