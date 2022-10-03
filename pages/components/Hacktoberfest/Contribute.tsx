d4f6df8f8f32f670392b3f3556a7d8a10cc51bdc
codeswithroh <codeswithroh@gmail.com>
2022-10-04 02:40:29 +0530

    fixed issue #198 and #196

--- pages/components/Hacktoberfest/Contribute.tsx
+++ pages/components/Hacktoberfest/Contribute.tsx
@@ -1,21 +1,32 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Contribute = () => {
-    return (
-        <div>
-            <div className='justify-center items-center space-x-5 space-y-3 md:grid lg:grid-cols-2 mx-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]'>
-                <div className='space-y-7'>
-                    <h1 className='text-[24px] lg:text-3xl xl:text-4xl'>How to Contribute ❓🤔</h1>
-                    <p className='text-[14px] '>You will get the idea of how to contribute by visiting the contribution.md file of each repo.</p>
-                    <button className='btn-blue mt-6'>Read More</button>
-                </div>
-                <Link href='#' passHref>
-                    <img src='/contribute.png' className='mx-auto ' />
-                </Link>
-            </div>
+  return (
+    <div>
+      <div className="justify-center items-center space-x-5 space-y-3 md:grid lg:grid-cols-2 mx-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]">
+        <div className="space-y-7">
+          <h1 className="text-[24px] lg:text-3xl xl:text-4xl">
+            How to Contribute ❓🤔
+          </h1>
+          <p className="text-[14px] ">
+            You will get the idea of how to contribute by visiting the
+            contribution.md file of each repo.
+          </p>
+          <a
+            href="https://github.com/Clueless-Community/clueless-official-website/blob/main/CONTRIBUTING.md"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn-blue mt-6">Read More</button>
+          </a>
         </div>
-    )
-}
+        <Link href="#" passHref>
+          <img src="/contribute.png" className="mx-auto " />
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Contribute
+export default Contribute;
--- pages/components/Hacktoberfest/Guidelines.tsx
+++ pages/components/Hacktoberfest/Guidelines.tsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const Guidelines = () => {
-    return (
-        <div className=''>
-            <h1 className='text-center text-[24px] lg:text-3xl xl:text-4xl'>Guidelines ✅</h1>
-            <div className='space-x-5 space-y-3 md:grid lg:grid-cols-2 m-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]'>
-                <div className=''>
-                    <ol className='list-disc text-[14px] sm:text-lg lg:text-2xl space-y-1 md:space-y-3'>
-                        <li>Top Contributor will get Exclusive t-shirts for free</li>
-                        <li>All contributors will receive certificate for contributing</li>
-                        <li>Contribute to open-source and be the part of the revolution</li>
-                    </ol>
-                    <button className='btn-blue mt-6'>Read More</button>
-                </div>
-                <img src='/guidelines.png' className='mx-auto md:mx-0' />
-
-            </div>
+  return (
+    <div className="">
+      <h1 className="text-center text-[24px] lg:text-3xl xl:text-4xl">
+        Guidelines ✅
+      </h1>
+      <div className="space-x-5 space-y-3 md:grid lg:grid-cols-2 m-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]">
+        <div className="">
+          <ol className="list-disc text-[14px] sm:text-lg lg:text-2xl space-y-1 md:space-y-3">
+            <li>Top Contributor will get Exclusive t-shirts for free</li>
+            <li>All contributors will receive certificate for contributing</li>
+            <li>Contribute to open-source and be the part of the revolution</li>
+          </ol>
+          <a
+            href="https://github.com/Clueless-Community/clueless-official-website/blob/main/CODE_OF_CONDUCT.md"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btn-blue mt-6">Read More</button>
+          </a>
         </div>
-    )
-}
+        <img src="/guidelines.png" alt="..." className="mx-auto md:mx-0" />
+      </div>
+    </div>
+  );
+};
 
-export default Guidelines
+export default Guidelines;
