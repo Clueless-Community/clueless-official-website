d66da75604e5c1a65fa1cfb13ebc3ba8fe399985
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-10-05 02:00:32 +0530

    Swags Updated

--- pages/hacktoberfest.tsx
+++ pages/hacktoberfest.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import React from 'react'
 import Clueless_Hacktoberfest from './components/Hacktoberfest/Clueless_Hacktoberfest'
-import Contribute from './components/Hacktoberfest/Swags'
 import Guidelines from './components/Hacktoberfest/Guidelines'
 import Projects from './components/Hacktoberfest/Projects'
 import RewardsAndPerks from './components/Hacktoberfest/RewardsAndPerks'
 import Why_Contribute from './components/Hacktoberfest/Why_Contribute'
 import Footer from './components/shared/Footer'
 import Navbar from './components/shared/Navbar/Navbar'
+import Swags from './components/Hacktoberfest/Swags'
 
 const hacktoberfest = () => {
     return (
@@ -19,7 +19,7 @@ const hacktoberfest = () => {
             <Clueless_Hacktoberfest />
             <RewardsAndPerks />
             <Projects />
-            <Contribute />
+            <Swags />
             <Why_Contribute />
             <Guidelines />
             <Footer />
