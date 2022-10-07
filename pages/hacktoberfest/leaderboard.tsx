dd1629f57fe3dd8454198c1d449b4597ba7cca60
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-10-08 01:51:41 +0530

    LeaderBoard Updated with PR_merged key

--- pages/hacktoberfest/leaderboard.tsx
+++ pages/hacktoberfest/leaderboard.tsx
@@ -44,6 +44,7 @@ const leaderboard: React.FC = () => {
                         <tr className='text-left child:py-2 child:px-2'>
                             <th className='xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm'>No</th>
                             <th>Name</th>
+                            <th>PR Merged</th>
                             <th className='xl:rounded-tr-md rounded-tr-sm xl:rounded-br-md rounded-br-sm w-fit'>Points</th>
                         </tr>
                     </thead>
@@ -53,6 +54,7 @@ const leaderboard: React.FC = () => {
                                 <tr className={`${i % 2 === 0 && 'bg-[#DBE0EB]'} rounded-md`} key={i}>
                                     <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold`}>{i + 1}.</td>
                                     <td className={`my-2 flex justify-start items-center xl:space-x-4 space-x-2 w-full`}><img src={data.avatar} className='w-16 rounded-full border-dashed border-2 border-blue-400' alt='' /><span>{data.name}</span></td>
+                                    <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold`}>{data.pr_merged}</td>
                                     <td className='my-2 pr-2 xl:rounded-tr-md rounded-tr-sm xl:rounded-br-md rounded-br-sm w-fit'><div className='flex justify-start items-center space-x-2'><img src="/leaderboardlogo.png" alt='leaderboardLogo' className='w-8' /><span className='float-left'>{data.points}</span></div></td>
                                 </tr>)
                         })}
