46c2c26771ffb5f057bf30bc47f3cf72b160860c
Rahul Karda <rahulkarda2002@gmail.com>
2022-10-07 09:38:24 +0530

    Update RewardsAndPerks.tsx UI
    The list bullets were overlapping with the image, adding some margin on the left will avoid this issue.

--- pages/components/Hacktoberfest/RewardsAndPerks.tsx
+++ pages/components/Hacktoberfest/RewardsAndPerks.tsx
@@ -7,7 +7,7 @@ const RewardsAndPerks = () => {
             <h1 className='text-center text-[24px] lg:text-3xl xl:text-4xl'>Rewards & Perks 🏆</h1>
             <div className='space-x-5 space-y-3 md:grid lg:grid-cols-2 m-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]'>
                 <img src='/rewards.png' className='mx-auto md:mx-0' />
-                <div className=''>
+                <div className='ml-12'>
                     <ol className='list-disc text-[14px] sm:text-lg lg:text-2xl space-y-1 md:space-y-3 my-auto'>
                         <li>Top Contributor will get Exclusive t-shirts for free</li>
                         <li>All contributors will receive certificate for contributing</li>
