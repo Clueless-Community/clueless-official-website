142241a5b0680a90999246e228678cf75ab2e741
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-10-09 10:51:14 +0530

    username truncated for mobile screen

--- pages/hacktoberfest/leaderboard.tsx
+++ pages/hacktoberfest/leaderboard.tsx
@@ -57,7 +57,7 @@ const Leaderboard: React.FC = () => {
                             return (
                                 <tr className={`${i % 2 === 0 && 'bg-[#DBE0EB]'} rounded-md`} key={i}>
                                     <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold`}>{i + 1}.</td>
-                                    <td className={`my-2 flex justify-start items-center xl:space-x-4 space-x-2 w-full`}><img src={data.avatar} className='w-16 rounded-full border-dashed border-2 border-blue-400 text-lg' alt='' /><span>{data.uname}</span></td>
+                                    <td className={`my-2 flex justify-start items-center xl:space-x-4 space-x-2 w-full`}><img src={data.avatar} className='w-16 rounded-full border-dashed border-2 border-blue-400 text-sm ' alt='' /><span className='max-w-[113px] truncate sm:max-w-fit'>{data.uname}</span></td>
                                     <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold text-center`}>{data.pr_merged}</td>
                                     <td className='my-2 pr-2 xl:rounded-tr-md rounded-tr-sm xl:rounded-br-md rounded-br-sm w-fit'><div className='flex justify-start items-center space-x-2'><img src="/leaderboardlogo.png" alt='leaderboardLogo' className='w-8' /><span className='float-left'>{data.points}</span></div></td>
                                 </tr>)
