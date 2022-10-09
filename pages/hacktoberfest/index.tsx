af77766922ff2f791a6e84ff2964e8dfab3c503f
Debajyoti Saha <debajyotisaha14@gmail.com>
2022-10-09 09:49:23 +0530

    Leaderboard enhanced

--- pages/hacktoberfest/index.tsx
+++ pages/hacktoberfest/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import React from 'react'
 import Clueless_Hacktoberfest from '../components/Hacktoberfest/Clueless_Hacktoberfest'
 import Guidelines from '../components/Hacktoberfest/Guidelines'
+import Leaderboard from '../components/Hacktoberfest/Leaderboard'
 import Projects from '../components/Hacktoberfest/Projects'
 import RewardsAndPerks from '../components/Hacktoberfest/RewardsAndPerks'
 import Swags from '../components/Hacktoberfest/Swags'
@@ -17,6 +18,7 @@ const hacktoberfest = () => {
                 <title>Hacktoberfest</title>
             </Head>
             <Navbar />
+            <Leaderboard />
             <Clueless_Hacktoberfest />
             <RewardsAndPerks />
             <Projects />
--- pages/hacktoberfest/leaderboard.tsx
+++ pages/hacktoberfest/leaderboard.tsx
@@ -10,7 +10,7 @@ const Leaderboard: React.FC = () => {
     const [leaderboard, setLeaderboard] = React.useState<ILeaderboardHacktoberfest[]>([]);
     const [loading, setLoading] = React.useState<boolean>(false);
 
-    console.log(leaderboard);
+    console.table(leaderboard);
 
 
     const handleLeaderboardFetch = React.useCallback(async () => {
@@ -44,17 +44,17 @@ const Leaderboard: React.FC = () => {
                         <tr className='text-left child:py-2 child:px-2'>
                             <th className='xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm'>No</th>
                             <th>Name</th>
-                            <th>PR Merged</th>
+                            <th className='text-center'>PR Merged</th>
                             <th className='xl:rounded-tr-md rounded-tr-sm xl:rounded-br-md rounded-br-sm w-fit'>Points</th>
                         </tr>
                     </thead>
-                    <tbody className=''>
+                    <tbody>
                         {leaderboard?.map((data, i) => {
                             return (
                                 <tr className={`${i % 2 === 0 && 'bg-[#DBE0EB]'} rounded-md`} key={i}>
                                     <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold`}>{i + 1}.</td>
-                                    <td className={`my-2 flex justify-start items-center xl:space-x-4 space-x-2 w-full`}><img src={data.avatar} className='w-16 rounded-full border-dashed border-2 border-blue-400' alt='' /><span>{data.name}</span></td>
-                                    <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold`}>{data.pr_merged}</td>
+                                    <td className={`my-2 flex justify-start items-center xl:space-x-4 space-x-2 w-full`}><img src={data.avatar} className='w-16 rounded-full border-dashed border-2 border-blue-400' alt='' /><span>{data.uname}</span></td>
+                                    <td className={`my-2 pl-2 xl:rounded-tl-md rounded-tl-sm xl:rounded-bl-md rounded-bl-sm font-semibold text-center`}>{data.pr_merged}</td>
                                     <td className='my-2 pr-2 xl:rounded-tr-md rounded-tr-sm xl:rounded-br-md rounded-br-sm w-fit'><div className='flex justify-start items-center space-x-2'><img src="/leaderboardlogo.png" alt='leaderboardLogo' className='w-8' /><span className='float-left'>{data.points}</span></div></td>
                                 </tr>)
                         })}
