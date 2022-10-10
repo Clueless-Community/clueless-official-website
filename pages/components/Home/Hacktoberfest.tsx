f98ddf5df0186e4b17ebf6fe935d55ebccdaec01
Styles <kartik100100@gmail.com>
2022-10-10 15:58:24 +0530

    improved the hacktoberfest card with new hq svg logo

--- pages/components/Home/Hacktoberfest.tsx
+++ pages/components/Home/Hacktoberfest.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link';
 
 const Hacktoberfest = () => {
     return (
-        <div className='bg-[#1B1424] md:grid lg:grid-cols-2 m-8 md:mx-40 sm:m-20 flex flex-col text-center lg:text-left rounded-3xl py-[28px] px-[20px] md:px-[91px]'>
-            <img src='/hacktoberfest_logo.png' className='mx-auto lg:mx-0' />
+        <div className='bg-[#f5f0f7] md:grid lg:grid-cols-2 m-8 md:mx-40 sm:m-20 flex flex-col text-center lg:text-left rounded-3xl py-[28px] px-[20px] md:px-[91px] justify-center items-center'>
+            <img src='/hacktoberfest_2022.svg' className='mx-auto lg:mx-0' />
             <div className='my-auto'>
-                <h1 className='text-white text-[14px] lg:text-3xl xl:text-4xl'>Join the biggest Open-source festival with Clueless & Win Amazing Swags 🎊</h1>
+                <h1 className='text-black text-[14px] lg:text-3xl xl:text-4xl p-10'>Join the biggest Open-source festival with Clueless & Win Amazing Swags 🎊</h1>
                 <Link href="/hacktoberfest" passHref>
-                    <button className="btn-blue bg-white text-black hover:bg-gray-100 mt-10 flex items-center mx-auto lg:mx-0">
+                    <button className="btn-blue bg-white text-black hover:bg-gray-100 flex mx-auto lg:mx-10">
                         <ReadMoreIcon className="mr-2" />Explore
                     </button>
                 </Link>
