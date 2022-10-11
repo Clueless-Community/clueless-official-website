9e6ed8c2a5e03923fbd7b1339cbfafd44445867a
Dev jain <69387311+devjain2309@users.noreply.github.com>
2022-10-11 16:54:42 +0530

    Update ProjectCard.tsx

--- pages/components/Hacktoberfest/ProjectCard.tsx
+++ pages/components/Hacktoberfest/ProjectCard.tsx
@@ -29,31 +29,6 @@ const ProjectCard: React.FC<ProjectProps> = ({ name, description, imgSources, re
                     <ReadMoreIcon className="mr-2" />Explore
                 </button>
             </Link>
-
-const ProjectCard: React.FC<ProjectProps> = ({
-  name,
-  description,
-  imgSources,
-  repoUrl,
-}) => {
-  return (
-    <div className="md:w-[475px] xl:w-[350px] bg-gradient-to-r from-black via-black to-neutral-400 rounded-2xl p-[57px]">
-      <h1 className="text-white text-[24px] lg:text-3xl">{name}</h1>
-      <div className="space-y-2">
-        <p className="text-white text-sm mt-3">{description}</p>
-        <div className="space-x-3 flex justify-center md:justify-center lg:justify-start">
-          {imgSources?.map((image, i) => {
-            return <img key={i} src={image} className="rounded-full w-5 h-5" />;
-          })}
-
-        </div>
-      </div>
-      <Link href={`${repoUrl}`} passHref>
-        <button className="btn-blue bg-white text-black hover:bg-gray-100 mt-10 flex items-center mx-auto lg:mx-0">
-          <ReadMoreIcon className="mr-2" />
-          Explore
-        </button>
-      </Link>
     </div>
   );
 };
