17a3347243ec7ad48105eedf9f40ff29364d87a8
Subham <subhambhattacharya700@gmail.com>
2022-10-13 00:35:24 +0530

    Logout button repositioning in mobile view & Logout confirmation dialog #228

---
+++ pages/components/shared/hooks/useConfirmation.tsx
@@ -0,0 +1,67 @@
+import React, { useMemo, useCallback } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+type CloseReason = "backdropClick" | "escapeKeyDown" | "agree" | "disAgree";
+interface DialogData {
+  title: string;
+  subTitle: string;
+}
+
+const useConfirmationDialog = (
+  { title, subTitle }: DialogData,
+  onAgree: () => any,
+  onDisAgree?: (reason?: string) => any
+) => {
+  const [open, setOpen] = React.useState(false);
+
+  const openDialog = useCallback(() => setOpen(true), []);
+  const handleClose = useCallback((ev: any, reason?: CloseReason) => {
+    setOpen(false);
+    reason === "agree" ? onAgree() : onDisAgree?.(reason);
+  }, []);
+  const handleActionsClick = (...args: any) => handleClose(...args.reverse());
+
+  const dialog = useMemo(
+    () => (
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {subTitle}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button
+            type="button"
+            className="btn-muted text-xs sm:text-lg "
+            onClick={handleActionsClick.bind(this, "disAgree")}
+            autoFocus
+          >
+            Cancel
+          </button>
+          <button
+            onClick={handleActionsClick.bind(this, "agree")}
+            type="button"
+            className="btn-blue text-xs sm:text-lg "
+          >
+            Ok
+          </button>
+        </DialogActions>
+      </Dialog>
+    ),
+    [open]
+  );
+
+  return [dialog, openDialog];
+};
+
+export default useConfirmationDialog;
