180694adc3645f68a307be19ebbb735921a18113
Raj Kanani <rajkanani1206@gmail.com>
2022-10-13 10:23:20 +0530

    minor change

--- pages/components/Hacktoberfest/RewardsAndPerks.tsx
+++ pages/components/Hacktoberfest/RewardsAndPerks.tsx
@@ -5,7 +5,7 @@ const RewardsAndPerks = () => {
   return (
     <div>
       <h1 className="text-center text-[24px] lg:text-3xl xl:text-4xl">Rewards & Perks 🏆</h1>
-      <div className="space-x-5 space-y-3 md:grid lg:grid-cols-2 m-10 md:mx-40 flex flex-col text-center lg:text-left py-[28px] px-[20px] sm:px-[91px]">
+      <div className="space-x-5 space-y-3 md:grid lg:grid-cols-2 m-10 md:mx-40 flex flex-col text-center lg:text-left pb-10 px-[20px] sm:px-[91px]">
         <img src="/rewards.png" className="mx-auto md:mx-0" />
         <div className="ml-12 pt-8">
           <ol className="list-disc text-[14px] sm:text-lg lg:text-2xl space-y-1 md:space-y-3 my-auto">
