94125ef5cf1b9727c24746895d1e827100935f37
prateekme70 <114897956+prateekme70@users.noreply.github.com>
2022-10-18 10:12:23 +0530

    Update theme.ts
    this would be better...

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,11 +3,11 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
     breakpoints: {
       values: {
-        xs: 0,
-        sm: 600,
-        md: 900,
-        lg: 1200,
-        xl: 1536,
+        xs: 10,
+        sm: 650,
+        md: 1000,
+        lg: 1350,
+        xl: 1736,
       },
     },
   });
