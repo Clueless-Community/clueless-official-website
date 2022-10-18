ea18dc990256a38a5722b3794bafc9ee3c7a206e
kingshuk3006 <kingsarkar3006@gmail.com>
2022-10-19 02:13:24 +0530

    ProfileCard-Elevation

--- pages/components/AboutUS/ProfileCard.tsx
+++ pages/components/AboutUS/ProfileCard.tsx
@@ -16,18 +16,21 @@ type Props = {
 }
 
 
-const ProfileCard: React.FC<Props> = ({key, img, username, tag, about, github, linkedin, twitter}) =>{
-    return(
-        <div className="flex flex-col h-96 w-72 items-center py-7 px-5 border-2 border-dashed rounded-xl border-black border-opacity-30" key={key}>
-            <img className="h-20 w-20 my-3 rounded-full" src={img} alt="" />
-            <h1 className="font-raleway font-semibold mt-2 text-center">{username}</h1>
-            <h2 className="font-raleway text-[#7E7E7E] text-xs mt-1 mb-2">{tag}</h2>
-            <p className="h-20 font-nunito font-semibold text-sm mt-5 mb-4 text-center">{about}</p>
-            <div className="mt-8 w-4/5 flex justify-between">
-                <a href={github} target="_blank" rel="noreferrer"><GitHubIcon/></a>
-                <a href={linkedin} target="_blank" rel="noreferrer"><LinkedInIcon/></a>
-                <a href={twitter} target="_blank" rel="noreferrer"><TwitterIcon/></a>
+const ProfileCard: React.FC<Props> = ({ key, img, username, tag, about, github, linkedin, twitter }) => {
+    return (
+        <div className="group flex flex-col h-96 w-72 items-center justify-center py-7 px-5 border-2 border-dashed hover:border-solid hover:scale-105 duration-200 hover:bg-[#f2f2f2ad] rounded-xl border-black border-opacity-30 " key={key}>
+            <img className="group-hover:h-20 group-hover:w-20 w-24 h-24 my-3 rounded-full object-cover" src={img} alt="" />
+            <h1 className="font-raleway font-semibold mt-2 text-center text-lg group-hover:text-base">{username}</h1>
+            <h2 className="font-raleway text-[#7E7E7E] text-sm group-hover:text-xs mt-1 mb-2">{tag}</h2>
+            <div className="hidden group-hover:block">
+                <p className="h-20 font-nunito font-semibold text-sm mt-5 mb-4 text-center">{about}</p>
             </div>
+            <div className="group-hover:mt-8 w-4/5 flex justify-center space-x-4 group-hover:justify-between">
+                <a href={github} target="_blank" rel="noreferrer"><GitHubIcon /></a>
+                <a href={linkedin} target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+                <a href={twitter} target="_blank" rel="noreferrer"><TwitterIcon /></a>
+            </div>
+
         </div>
     )
 
