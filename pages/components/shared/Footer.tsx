27b5d2b4b3bee1084c57cc941bec6e9c940bb446
Luciane <lucianefernandescdf@gmail.com>
2022-10-19 20:17:15 -0300

    change footer title and link

--- pages/components/shared/Footer.tsx
+++ pages/components/shared/Footer.tsx
@@ -37,11 +37,11 @@ const Footer: React.FC = () => {
         <div className="flex flex-col mx-auto  my-5 sm:my-0 sm:text-left text-center">
           <h3 className="font-nunito font-semibold text-xl mb-8">Quick links</h3>
           <Link href="/challenges-events">
-            <a className="my-3 hover:text-skin-main transition-all">Events & Challenges</a>
+            <a className="my-3 hover:text-skin-main transition-all">Events</a>
           </Link>
           <a
             className="my-3 hover:text-skin-main transition-all"
-            href="https://clueless-resources.super.site/"
+            href="https://www.clueless.tech/events"
             target="_blank"
             rel="noreferrer"
           >
