eeb6d7d335e5e6a4142ae565c51ce6563d43b9ce
rohan9024 <rohanwandre24@gmail.com>
2022-10-20 14:12:43 +0530

    Redesigned the login page

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import GoogleIcon from "@mui/icons-material/Google";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import { getProviders, signIn } from "next-auth/react"
-import { GetServerSideProps } from 'next';
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import { getProviders, signIn } from "next-auth/react";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import Navbar from "../components/shared/Navbar/Navbar";
 
 const signin: React.FC = ({ providers }: any) => {
   return (
@@ -16,26 +17,48 @@ const signin: React.FC = ({ providers }: any) => {
         <title>🔒ClueLess | LogIn</title>
       </Head>
       <>
-      <img src="/sidelines-signin.png" alt="sidelines-signin" className=" absolute left-0 top-0 hidden lg:block scale-75" />
-      <img src="/sidelines-signin.png" alt="sidelines-signin" className=" absolute right-0 bottom-0 rotate-180 scale-75 hidden lg:block" />
-        <div className="min-h-screen flex justify-center items-center flex-col" >
-          <div className="">
-            <h1 className="text-4xl text-center">Welcome to <span className="text-skin-main">Clueless <br /> Community</span></h1>
+        <Navbar />
+        <div className="min-h-screen  flex flex-col lg:flex-row justify-center items-center bg-gray-100 space-y-12 lg:space-y-0">
+          {/* Sign In Text and image */}
+          <div className="relative lg:mb-44 ">
+            <div className="absolute top-0 lg:-right-64 -right-4 w-44 h-44 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-50" />
           </div>
-          <div className="flex items-center">
-            <div className=" hidden md:flex">
-              <img src="/signin-page-laptop-2.png" alt="signin-page-laptop" className="w-[600px] h-[500px] object-cover cursor-pointer" />
-            </div>
-            <div>
-              <img src="/ClueLess Logo.png" alt="" className="block md:hidden mx-auto w-[90px] mb-6" />
+          <div className=" flex flex-col justify-start items-center space-y-6 lg:mb-16">
+            <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold text-left w-full">
+              Sign In to
+            </h1>
+            <h1 className="text-skin-main text-4xl lg:text-5xl xl:text-6xl font-bold text-left w-full">
+              Clueless Community
+            </h1>
+          </div>
+          <div className="flex">
+            <img
+              src="/loginRocket.PNG"
+              alt="signin-page-laptop"
+              className="w-[400px] h-[300px] object-contain cursor-pointer"
+            />
+          </div>
+
+          <div>
+            <div className="flex flex-col items-center">
               <div className="space-y-6">
-                <button className="px-24 py-3 w-full text-lg flex items-center justify-center font-semibold bg-[#0276E1] hover:bg-[#0085FF] text-white max-w-[301px] rounded mx-auto cursor-pointer" onClick={(): void => { signIn(providers.google.id, { callbackUrl: "/usercheck" }) }}>
+                <button
+                  className="shadow-lg px-24 py-3 w-full text-lg flex items-center justify-center font-semibold bg-[#0276E1] hover:bg-[#0085FF] text-white max-w-[301px] rounded mx-auto hover:duration-150 hover:ease-in-out hover:scale-105 transition hover:-translate-y-2 hover:cursor-pointer"
+                  onClick={(): void => {
+                    signIn(providers.google.id, { callbackUrl: "/usercheck" });
+                  }}
+                >
                   <div className="flex justify-center space-x-2">
                     <GoogleIcon />
                     <p>Google</p>
                   </div>
                 </button>
-                <button className="px-4 py-3 w-full font-semibold text-lg flex items-center justify-center bg-[#373636] hover:bg-[#262626] text-white max-w-[301px] rounded mx-auto cursor-pointer" onClick={(): void => { signIn(providers.github.id, { callbackUrl: "/usercheck" }) }}>
+                <button
+                  className="shadow-lg px-4 py-3 w-full font-semibold text-lg flex items-center justify-center bg-[#373636] hover:bg-[#262626] text-white max-w-[301px] rounded mx-auto hover:duration-150 hover:ease-in-out hover:scale-105 transition hover:-translate-y-2 hover:cursor-pointer"
+                  onClick={(): void => {
+                    signIn(providers.github.id, { callbackUrl: "/usercheck" });
+                  }}
+                >
                   <div className="flex justify-center space-x-2">
                     <GitHubIcon />
                     <p>GitHub</p>
@@ -49,12 +72,16 @@ const signin: React.FC = ({ providers }: any) => {
                 </button> */}
               </div>
               <Link href="/" passHref>
-                <p className=" text-center text-skin-main font-semibold hover:scale-105 hover:-translate-y-1 transition hover:ease-in-out cursor-pointer mt-10"><p className="flex items-center justify-center"><KeyboardArrowLeftIcon />Go back Home</p></p>
+                <p className=" text-center text-skin-main font-semibold hover:scale-105 hover:-translate-y-1 transition hover:ease-in-out cursor-pointer mt-10">
+                  <p className="flex items-center justify-center">
+                    <KeyboardArrowLeftIcon />
+                    Go back Home
+                  </p>
+                </p>
               </Link>
             </div>
           </div>
-          <div className="scale-50">
-          </div>
+          <div className="scale-50"></div>
         </div>
       </>
     </React.Fragment>
@@ -64,10 +91,10 @@ const signin: React.FC = ({ providers }: any) => {
 export default signin;
 
 export const getServerSideProps: GetServerSideProps = async () => {
-  const providers = await getProviders()
+  const providers = await getProviders();
   return {
     props: {
-      providers
-    }
-  }
-}
+      providers,
+    },
+  };
+};
