71036ea8ad39bfca344099290f10071a44890328
Durgesh <durgeshmahajan1722@gmail.com>
2022-10-30 00:35:15 +0530

    Fixed the icons position in What is Clueless section

--- pages/components/Home/WhatIsClueLess.tsx
+++ pages/components/Home/WhatIsClueLess.tsx
@@ -32,7 +32,7 @@ const WhatIsClueLess: React.FC = () => {
           }}
         />
         <StorageIcon
-          className="absolute text-5xl hover:scale-125 transition-all cursor-pointer hover:text-skin-main text-skin-hoverBlue  md:block top-12 2xl:left-60 xl:left-28 left-10 sm:left-24"
+          className="absolute text-5xl hover:scale-125 transition-all cursor-pointer hover:text-skin-main text-skin-hoverBlue  md:block top-12 2xl:left-40 xl:left-28 left-10 sm:left-24"
           fontSize='inherit' sx={{ fontSize: "50px" }}
           onMouseOver={() => {
             setState(2);
@@ -42,7 +42,7 @@ const WhatIsClueLess: React.FC = () => {
           }}
         />
         <LocalPoliceIcon
-          className="absolute text-5xl hover:scale-125 transition-all cursor-pointer hover:text-skin-main text-skin-hoverBlue  md:block top-12 2xl:right-72 xl:right-40 sm:right-24 right-10"
+          className="absolute text-5xl hover:scale-125 transition-all cursor-pointer hover:text-skin-main text-skin-hoverBlue  md:block top-12 2xl:right-52 xl:right-40 sm:right-24 right-10"
           fontSize='inherit' sx={{ fontSize: "50px" }}
           onMouseOver={() => {
             setState(3);
