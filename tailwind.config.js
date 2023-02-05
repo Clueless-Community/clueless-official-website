97a267e7293a9d052d9c6a7d6d4b903fd3ba7669
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2023-02-05 22:41:45 +0530

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
