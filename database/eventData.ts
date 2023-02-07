08b3fbfdc9fe9b4819867f8d4eea59929c4c6603
Arya <aryapratimray@gmail.com>
2023-02-08 01:06:09 +0530

    Events Card change

---
+++ database/eventData.ts
@@ -0,0 +1,23 @@
+const data = [
+    {
+      project_id: "1",
+      event_icon_image: "/flutter_kolkata.jpeg",
+      event_name: "Flutter Festival",
+      time_period: "12:00 - 4:00",
+      agenda: "Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities",
+      date: "1st Feb - 31st March ",
+      // speakers_info: "uwshge",
+    },
+    {
+        project_id: "2",
+        event_icon_image: "/flutter_kolkata.jpeg",
+        event_name: "JWOC",
+        time_period: "12:00 - 4:00",
+        agenda: "Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities",
+        date: "https://www.linkedin.com/in/rajdeep-sengupta/",
+        // speakers_info: "we7uyhgu",
+      },
+  ];
+  
+  export default data;
+  
--- pages/components/Home/Events/UpcomingEvents.tsx
+++ pages/components/Home/Events/UpcomingEvents.tsx
@@ -5,42 +5,41 @@ import { db } from '../../../../lib/clientApp';
 import UpcomingEventCard from './UpcomingEventCard';
 import { format } from 'date-fns';
 import { CircularProgress } from '@mui/material';
-
+import eventData from '../../../../database/eventData'
 
 
 
 const UpcomingEvents = () => {
-  const router = useRouter()
-  const [eventData, seteventData] = React.useState<any>([])
+  // const router = useRouter()
+  // const [eventData, seteventData] = React.useState<any>([])
 
-  const getEventData = async () => {
-    const eventRef = collection(db, 'events',)
-    const eventSnap = await getDocs(eventRef);
-    eventSnap.forEach(doc => {
-      seteventData((event: any) => { return [{ project_id: doc.id, ...doc.data() }, ...event] })
-    })
-  }
+  // const getEventData = async () => {
+  //   const eventRef = collection(db, 'events',)
+  //   const eventSnap = await getDocs(eventRef);
+  //   eventSnap.forEach(doc => {
+  //     seteventData((event: any) => { return [{ project_id: doc.id, ...doc.data() }, ...event] })
+  //   })
+  // }
 
-  console.log(eventData);
+  // console.log(eventData);
 
 
-  React.useEffect(() => {
-    if (!router.isReady) return;
-    getEventData();
-  }, [router.isReady])
+  // React.useEffect(() => {
+  //   if (!router.isReady) return;
+  //   getEventData();
+  // }, [router.isReady])
 
   return (
 
     <div className='xl:my-20 my-4 xl:mx-40 md:mx-20 mx-4'>
-      <h1 className='text-[#7EE787] text-6xl font-medium leading-[70.5px]' >Ongoing Events </h1>
+      <h1 className='text-[#7EE787] text-6xl font-semibold leading-[70.5px]' >Ongoing Events </h1>
       <div>
         {eventData.map((data: any, i: number) => {
-          const date = format(new Date(data.date.seconds * 1000), 'do LLLLLL, yyyy')
+          // const date = format(new Date(data.date.seconds * 1000), 'do LLLLLL, yyyy')
           return (
             <div key={i} className="my-10">
 
-              <UpcomingEventCard eventposter={data.event_icon_image} heading={data.event_name} venue={data.venue_name} Time={data.time_period} instructorOrspeaker={data.speakers_info}
-                attractions="Win T-shirts, swags and free food. 🚀 " agenda={data.agenda} eventId={data.project_id} date={date} />
+              <UpcomingEventCard eventposter={data.event_icon_image} heading={data.event_name} venue={data.venue_name} Time={data.time_period} instructorOrspeaker={data.speakers_info} agenda={data.agenda} eventId={data.project_id} date={data.date} />
             </div>
 
           )
