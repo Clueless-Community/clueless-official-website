bbba1e2bda29b5b57b790259e7f288e5c59c1837
Arya <aryapratimray@gmail.com>
2023-02-12 22:51:40 +0530

    bug fixes

--- database/eventData.ts
+++ database/eventData.ts
@@ -4,8 +4,7 @@ const data = [
     event_icon_image: '/flutter.webp',
     event_name: 'Flutter Festival',
     time_period: '12:00 - 4:00',
-    agenda:
-      'Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities',
+    desc: 'Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities',
     date: '1st Feb-31st Mar ',
     speakers_info: 'IamU',
   },
@@ -14,8 +13,7 @@ const data = [
     event_icon_image: '/flutter.webp',
     event_name: 'JWOC',
     time_period: '12:00 - 4:00',
-    agenda:
-      'Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities',
+    desc: 'Winter of Code is a program aimed to increase participation for the Google Summer of Code program among students in colleges and universities',
     date: '1st Feb-31st Mar',
     speakers_info: 'IamU',
   },
