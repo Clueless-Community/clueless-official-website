d23357da5a177a8151b29d6aa24b4e573818c162
Arya <aryapratimray@gmail.com>
2023-02-15 02:32:02 +0530

    code section

--- pages/components/home/Copilot/Copilot.tsx
+++ pages/components/home/Copilot/Copilot.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import LoopIcon from '@mui/icons-material/Loop';
 import { FaPython } from 'react-icons/fa';
@@ -6,6 +7,46 @@ import { IoLogoJavascript } from 'react-icons/io';
 import { SiGoland } from 'react-icons/si';
 
 const Copilot = () => {
+  const [active1, setActive1] = useState(true);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+
+  const ActivateButton1 = () => {
+    if (active1 === false) {
+      setActive1(!active1);
+    }
+    if (active2 === true) {
+      setActive2(!active2);
+    }
+    if (active3 === true) {
+      setActive3(!active3);
+    }
+  };
+
+  const ActivateButton2 = () => {
+    if (active2 === false) {
+      setActive2(!active2);
+    }
+    if (active1 === true) {
+      setActive1(!active1);
+    }
+    if (active3 === true) {
+      setActive3(!active3);
+    }
+  };
+
+  const ActivateButton3 = () => {
+    if (active3 === false) {
+      setActive3(!active3);
+    }
+    if (active1 === true) {
+      setActive1(!active1);
+    }
+    if (active2 === true) {
+      setActive2(!active2);
+    }
+  };
+
   return (
     <div className="my-[150px] xl:ml-[105px] md:mx-20 mx-[20px]">
       <h1 className="text-[#7EE787] xl:text-[55px] font-semibold xl:leading-[70.5px] leading-[30.5px] my-[55px] text-[36px]  xl:mr-[516px]">
@@ -13,46 +54,73 @@ const Copilot = () => {
       </h1>
       <div className="flex flex-row h-[519px]">
         <div className="w-[800px]">
-          <div className="h-[50px] flex flex-row bg-black rounded-t-[31px]">
-            <button className="h-[100%] flex flex-row rounded-tl-[31px]">
-              <FaPython className="text-[#b6b5b5] ml-[10px] mt-[15px]" />
-              <h1 className="text-[#b6b5b5] ml-[5px] mt-[10px] mr-[10px]">
+          <div className="h-[50px] flex flex-row bg-[#312b32] rounded-t-[31px]">
+            <button
+              onClick={ActivateButton1}
+              className={`mt-[10px] flex flex-row rounded-tr-[10px] rounded-tl-[19px] ${
+                active1 === true ? 'bg-black' : 'bg-[#312b32]'
+              }`}
+            >
+              <FaPython
+                className={` ml-[10px] mt-[14px] ${
+                  active1 === true ? 'text-white' : 'text-[#b6b5b5]'
+                }`}
+              />
+              <h1 className="text-[#b6b5b5] ml-[5px] mt-[9px] mr-[10px]">
                 draw_scatterplot.py
               </h1>
             </button>
-            <button className="h-[100%] flex flex-row">
-              <IoLogoJavascript className="text-[#b6b5b5] ml-[10px] mt-[15px]" />
-              <h1 className="text-[#b6b5b5] ml-[5px] mt-[10px] mr-[10px]">
+            <button
+              onClick={ActivateButton2}
+              className={`mt-[10px] flex flex-row rounded-t-[10px] ${
+                active2 === true ? 'bg-black' : 'bg-[#312b32]'
+              }`}
+            >
+              <IoLogoJavascript
+                className={` ml-[10px] mt-[14px] ${
+                  active2 === true ? 'text-white' : 'text-[#b6b5b5]'
+                }`}
+              />
+              <h1 className="text-[#b6b5b5] ml-[5px] mt-[9px] mr-[10px]">
                 time.js
               </h1>
             </button>
-            <button className="h-[100%] flex flex-row">
-              <SiGoland className="text-[#b6b5b5] ml-[10px] mt-[15px]" />
-              <h1 className="text-[#b6b5b5] ml-[5px] mt-[10px] mr-[10px]">
+            <button
+              onClick={ActivateButton3}
+              className={`mt-[10px] flex flex-row rounded-t-[10px] ${
+                active3 === true ? 'bg-black' : 'bg-[#312b32]'
+              }`}
+            >
+              <SiGoland
+                className={` ml-[10px] mt-[14px] ${
+                  active3 === true ? 'text-white' : 'text-[#b6b5b5]'
+                }`}
+              />
+              <h1 className="text-[#b6b5b5] ml-[5px] mt-[9px] mr-[10px]">
                 memoize.go
               </h1>
             </button>
           </div>
-          <div className="h-[375px] bg-[#312b32]"></div>
+          <div className="h-[375px] bg-black"></div>
           <div className="h-[93px] flex flex-row ">
-            <button className="w-[667px] flex flex-row bg-black rounded-b-[31px]">
+            <button className="w-[667px] flex flex-row bg-[#262227] rounded-b-[31px]">
               <LoopIcon className="text-[30px] mt-[30px] ml-[273px] text-white" />
               <h1 className="text-[26px] mt-[27px] text-white">Replay</h1>
             </button>
           </div>
         </div>
         <div className="w-[800px]">
-          <div className="h-[257px] mt-[120px] mr-[20px] ml-[75px]">
-            <h1 className="text-[26px] mb-[20px] font-medium">
+          <div className="h-[257px] mt-[80px] mr-[20px] ml-[75px]">
+            <h1 className="text-[26px] mb-[20px] font-medium text-white">
               Built with the vision to encourage open-source enthusiasts!
             </h1>
-            <h1 className="text-[26px] font-medium">
+            <h1 className="text-[26px] font-medium text-white">
               built with the vision to encourage open-source enthusiasts! built
               with the vision to encourage open-source enthusiasts!
             </h1>
           </div>
           <div className="ml-[75px]">
-            <button className="bg-[#FFFFFF] text-[#7EE787] flex flex-row">
+            <button className="text-[#7EE787] flex flex-row">
               <h1 className="text-[22px] font-extrabold">Explore</h1>
               <ArrowForwardIosIcon className="text-[23px] font-extrabold mt-[6px]" />
             </button>
