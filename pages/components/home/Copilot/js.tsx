a619c140707d87e5ac4d2c78efe3a1b201bc56a2
CS50X-RGB <rohanchatterjee866@gmail.com>
2023-02-23 19:14:20 +0530

    Done the changes

---
+++ pages/components/home/Copilot/js.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { TfiGithub } from 'react-icons/tfi';
+import { motion } from 'framer-motion';
+
+const Js = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="text-white pl-[18px] pb-[5px] mt-[30px]">
+          <span className="text-[#c92828]">const</span> seconds{' '}
+          <span className="text-[#73b2ed]">= 3600</span>{' '}
+        </h1>
+        <h1 className="text-white pl-[18px] pb-[5px] typewriter">
+          <span className="text-[#c92828]">const </span>
+          minutes
+          <span className="text-[#73b2ed]"> = </span>
+          seconds
+          <span className="text-[#73b2ed]"> / 60</span>
+        </h1>
+      </div>
+      <motion.div
+        whileInView={{ y: [10, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.8, delay: 0.7 }}
+      >
+        <div className="border-l-[3px] border-blue-600">
+          <div className="bg-blue-600/[0.25]">
+            <h1 className="text-white pl-[15px] pb-[5px]">
+              <span className="text-[#c92828]">const </span>
+              hours
+              <span className="text-[#73b2ed]"> = </span>
+              minutes
+              <span className="text-[#73b2ed]"> / 60</span>
+            </h1>
+          </div>
+          <div className="bg-blue-600/[0.25]">
+            <h1 className="text-white pl-[15px] pb-[5px]">
+              <span className="text-[#c92828]">const </span>
+              days
+              <span className="text-[#73b2ed]"> = </span>
+              hours
+              <span className="text-[#73b2ed]"> / 24</span>
+            </h1>
+          </div>
+          <div className="bg-blue-600/[0.25]">
+            <h1 className="text-white pl-[15px] pb-[5px]">
+              <span className="text-[#c92828]">const </span>
+              weeks
+              <span className="text-[#73b2ed]"> = </span>
+              days
+              <span className="text-[#73b2ed]"> / 7</span>
+            </h1>
+          </div>
+          <div className="bg-blue-600/[0.25]">
+            <h1 className="text-white pl-[15px] pb-[5px]">
+              <span className="text-[#c92828]">const </span>
+              months
+              <span className="text-[#73b2ed]"> = </span>
+              days
+              <span className="text-[#73b2ed]"> / 30</span>
+            </h1>
+          </div>
+          <div className="bg-blue-600/[0.25]">
+            <h1 className="text-white pl-[15px] pb-[5px]">
+              <span className="text-[#c92828]">const </span>
+              years
+              <span className="text-[#73b2ed]"> = </span>
+              months
+              <span className="text-[#73b2ed]"> / 12</span>
+            </h1>
+          </div>
+        </div>
+        <div className="rounded-[5px] w-[83px] h-[37px] flex flex-row bg-blue-600">
+          <TfiGithub className="text-white mt-[10px] ml-[5px]" />
+          <h1 className="text-white pt-[7px] ml-[2px]">Copilot</h1>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Js;
