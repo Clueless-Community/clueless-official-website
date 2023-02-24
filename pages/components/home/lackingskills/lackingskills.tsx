aaa6c5b9c36886e965212c07914193438cf1f1b0
amdpt323 <samandeep21@iitk.ac.in>
2023-02-25 01:09:56 +0530

    added media query and fixed button

--- pages/components/home/lackingskills/lackingskills.tsx
+++ pages/components/home/lackingskills/lackingskills.tsx
@@ -1,32 +1,22 @@
-import { Button, Grid } from '@mui/material';
-import { Stack } from '@mui/system';
 import React from 'react';
 
 const LackingSkills = () => {
   return (
     <main style={{ backgroundColor: '#1b1b1b', color: 'white' }}>
-      <Grid container spacing={4} padding={5}>
-        <Grid item md={10}>
-          <Stack className="md:w-2/5">
-            <h1 className="text-5xl text-[#72cf7b] pb-1">Lacking skills?</h1>
-            <h1 className="text-5xl pb-10 space-x-1 ">We got you covered.</h1>
-            <h4 className="pb-10 text-md">
-              built with the vision to encourage open-source enthusiasts built
-              with the vision to encourage open-source enthusiasts
-            </h4>
-            <Button
-              variant="contained"
-              className="md:w-52 bg-[#72cf7b] hover:bg-[#72cf7b]"
-            >
-              Explore resource hub
-            </Button>
-          </Stack>
-        </Grid>
-        <Grid item md={2}>
-          svg here
-        </Grid>
-      </Grid>
-      <div className="h-[56vh] relative">
+      <div>
+        <div className="md:w-2/5 p-5">
+          <h1 className="text-5xl text-[#72cf7b] pb-1">Lacking skills?</h1>
+          <h1 className="text-5xl pb-10 space-x-1 ">We got you covered.</h1>
+          <h4 className="pb-10 text-md">
+            built with the vision to encourage open-source enthusiasts built
+            with the vision to encourage open-source enthusiasts
+          </h4>
+          <button className="bg-[#72cf7b] p-2 rounded-md">
+            Explore resource hub
+          </button>
+        </div>
+      </div>
+      <div className="hidden lg:block h-[56vh] relative ">
         <h1 className="text-4xl text-[#72cf7b] absolute bottom-[36rem] left-[63rem]">
           App Dev
         </h1>
@@ -85,6 +75,71 @@ const LackingSkills = () => {
           />
         </svg>
         <h1 className="text-4xl text-[#72cf7b] absolute bottom-10 left-10 ">
+          DevOps
+        </h1>
+      </div>
+      <div className="h-[100%] lg:hidden relative">
+        <h1 className="text-4xl text-[#72cf7b] flex justify-center py-1">
+          App Dev
+        </h1>
+        <div className="flex justify-center">
+          <svg
+            width="73"
+            height="166"
+            viewBox="0 0 73 166"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3.92116 163.206C0.684609 159.083 3.64733 134.832 53.7524 107.351C71.9635 97.3622 79.2074 62.2013 54.7707 53.7179C25.3192 43.4935 19.3583 9.49871 18.1967 2.99973"
+              stroke="#7EE787"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-dasharray="20 20"
+            />
+          </svg>
+        </div>
+        <h1 className="text-4xl text-[#72cf7b] flex justify-center py-1">
+          Web 3
+        </h1>
+        <div className="flex justify-center">
+          <svg
+            width="39"
+            height="175"
+            viewBox="0 0 39 175"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M32 172C23.7738 164.496 17.5302 167.049 5.65946 135.605C-6.21134 104.161 17.5344 91.4042 26.9864 68.6368C46.1033 22.589 28.8693 13.199 23.8402 2.99989"
+              stroke="#E5E5E5"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-dasharray="20 20"
+            />
+          </svg>
+        </div>
+        <h1 className="text-4xl text-[#72cf7b] flex justify-center py-1">
+          Web Dev
+        </h1>
+        <div className="flex justify-center">
+          <svg
+            width="97"
+            height="233"
+            viewBox="0 0 97 233"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.5309 230.305C11.5294 226.311 20.4235 215.933 10.3768 196.289C-10.4512 155.566 10.8129 138.484 72.52 111.085C126.943 86.9204 60.1859 26.1007 67.9685 3.20752"
+              stroke="#7EE787"
+              stroke-width="5"
+              stroke-linecap="round"
+              stroke-dasharray="20 20"
+            />
+          </svg>
+        </div>
+        <h1 className="text-4xl text-[#72cf7b] flex justify-center py-1">
           DevOps
         </h1>
       </div>
