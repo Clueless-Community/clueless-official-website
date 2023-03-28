261723bd96ce7abf636a9db669e203d0059be7ad
Iron-Thunder <akashranipokhar@gmail.com>
2023-03-28 23:12:26 +0530

    added privacy policy page,  rendered image in error page, fixed all buttons and added input class field of contact us page in global css

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,21 +8,21 @@ const oops = () => {
         <div className="flex flex-col sm:mt-0 items-center h-fit  min-h-screen">
           <img
             className="mx-auto mt-11 h-[55vh] w-auto sm:mt-[1%] sm:h-[68vh] sm:w-auto flex flex-row items-center max-h-screen md:mt-[1.7%] "
-            src="404-img.png"
+            src="./LandingPage/404/404-img.png"
             alt=""
           />
           <h2 className=" text-white text-xl sm:text-2xl md:text-3xl mx-auto font-black ">
             OOPS! PAGE NOT FOUND
           </h2>
           <Link href="/">
-            <button className=" pl-[40px] pr-[40px] pb-[7px] pt-[7px] font-bold bg-green-400 hover:bg-green-500 rounded-[5px] mt-5">
+            <button className=" pl-[40px] pr-[40px] pb-[7px] pt-[7px] text-base items-center btn-green rounded-[5px] mt-5">
               Go Home
             </button>
           </Link>
         </div>
         <img
           className="absolute opacity-70 top-[-190px] left-[-190px] w-[700px]"
-          src="login-shade.png"
+          src="./LandingPage/shade/shade.png"
           alt=""
         />
       </div>
