6e94b6414515e8d14918d4b47b396b3001a44a2c
Ravindra Pandey <ravindrapandey99999@gmail.com>
2023-04-05 17:46:34 +0530

    navbar sticky and blur added

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -8,7 +8,7 @@ const PageLayout = ({ children }: Props) => {
   return (
     <div>
       <NavBar />
-      <div className="xl:px-24 md:px-10 px-5 overflow-x-hidden h-full pattern bg-[#1B1B1B] text-white">
+      <div className="xl:px-24 relative z-0 md:px-10 px-5 overflow-x-hidden h-full pattern bg-[#1B1B1B] text-white">
         {children}
       </div>
       <Footer />
