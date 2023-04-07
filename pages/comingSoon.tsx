93dd101bf3ad124a1fc09d5257f216df7226582e
Iron-Thunder <akashranipokhar@gmail.com>
2023-04-07 14:34:44 +0530

    added coming soon page

---
+++ pages/comingSoon.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const comingSoon = () => {
+  return (
+    <div>
+      <div className="flex flex-col-reverse min-h-[90vh]">
+        <div className=" flex items-center justify-center">
+          <img
+            className="sm:w-[95%] w-[800px] h-[150px] sm:h-auto md:w-[70%] mt-5"
+            src="/comingSoon/globe.png"
+            alt=""
+          />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <img
+            className="mt-[22vh] w-[90%] sm:w-[70%] sm:mt-[20vh] md:w-[45%] md:mt-[21vh]"
+            src="/comingSoon/img1.png"
+            alt=""
+          />
+
+          <img
+            className="sm:w-[65%] w-[80%] mt-[6%] mb-[6%] sm:mt-[3%] sm:mb-[3%] md:w-[40%] md:mt-0 md:mb-0"
+            src="/comingSoon/load.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default comingSoon;
